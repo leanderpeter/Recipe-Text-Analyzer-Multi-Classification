da747ebb7ae4be3ba64ac008268532fc5d938b25
Leander <kontakt@leanderpeter.de>
2021-04-01 11:02:39 +0200

    👍 Updated crawler algo for test and training .csv

--- web_crawler/get_recipes.py
+++ web_crawler/get_recipes.py
@@ -1,10 +1,9 @@
 import time
 import sys
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import csv
+import random
 
 #links to cuisine category
 cuisines = ["https://www.yummly.com/recipes?q=Best%20Chinese","https://www.yummly.com/recipes?q=Easy%20Chinese","https://www.yummly.com/recipes?q=Healthy%20Chinese",
@@ -15,10 +14,16 @@ cuisines = ["https://www.yummly.com/recipes?q=Best%20Chinese","https://www.yumml
 numbers_of_recipes = 50
 
 # desired dataset lenght
-d_lenght = 110
+d_lenght = 100
+
+#desired test lenght
+t_lenght = 10
 
 # dataset
 dataset = []
+
+# test dataset
+test_dataset = []
 
 # Init headless chrome 
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
@@ -59,24 +64,45 @@ for element in cuisines:
 # close browser after use
 driver.close()
 
+# create shuffled list of dataset with a lenght of k
+# test dataset
+d_shuffled = random.sample(dataset, k=t_lenght)
+
 # shorten dataset if necessary
 if len(dataset) > d_lenght:
 	dataset = dataset[:d_lenght]
 
-print("Dataset length: ", len(dataset))
+# print lenght of datasets
+print("Training dataset length: ", len(dataset))
+print("Test dataset length: ", len(d_shuffled))
+
 
 #write to csv
 csv_columns = ["cuisine", "name", "link"]
-csv_file = "dataset.csv"
+csv_file = "train_dataset.csv"
+csv_test_file = "test_dataset.csv"
 
 
 
 try:
+	''' Write Training Dataset to csv
+	'''
 	with open(csv_file, 'w') as csvfile:
 		writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
 		writer.writeheader()
 		for data in dataset:
 			writer.writerow(data)
-	print('Dataset successfully saved as: ', csv_file)
+	print('Train dataset successfully saved as: ', csv_file)
+
+	''' Write Test Dataset to csv
+	'''
+	with open(csv_test_file, 'w') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+		writer.writeheader()
+		for data in d_shuffled:
+			writer.writerow(data)
+	print('Test dataset successfully saved as: ', csv_test_file)
 except IOError:
 	print("I/O error")
+
+
